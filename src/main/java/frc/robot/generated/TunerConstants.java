a72e4b947baaf924eb1f8711a212e4f56304da8b
PsiFi96 <lt.theaker@icloud.com>
2025-12-01 17:47:24 -0800

    adjusted negatives in order to make rotation work

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -134,7 +134,7 @@ public class TunerConstants {
     private static final boolean kFrontLeftEncoderInverted = true;
 
     private static final Distance kFrontLeftXPos = Inches.of(12.375);
-    private static final Distance kFrontLeftYPos = Inches.of(11.3750025);
+    private static final Distance kFrontLeftYPos = Inches.of(-11.3750025);
 
     // Front Right
     private static final int kFrontRightDriveMotorId = 2;
@@ -145,7 +145,7 @@ public class TunerConstants {
     private static final boolean kFrontRightEncoderInverted = true;
 
     private static final Distance kFrontRightXPos = Inches.of(12.375);
-    private static final Distance kFrontRightYPos = Inches.of(-11.3750025);
+    private static final Distance kFrontRightYPos = Inches.of(11.3750025);
 
     // Back Left
     private static final int kBackLeftDriveMotorId = 3;
@@ -156,7 +156,7 @@ public class TunerConstants {
     private static final boolean kBackLeftEncoderInverted = true;
 
     private static final Distance kBackLeftXPos = Inches.of(-12.375);
-    private static final Distance kBackLeftYPos = Inches.of(11.3750025);
+    private static final Distance kBackLeftYPos = Inches.of(-11.3750025);
 
     // Back Right
     private static final int kBackRightDriveMotorId = 4;
@@ -167,7 +167,7 @@ public class TunerConstants {
     private static final boolean kBackRightEncoderInverted = true;
 
     private static final Distance kBackRightXPos = Inches.of(-12.375);
-    private static final Distance kBackRightYPos = Inches.of(-11.3750025);
+    private static final Distance kBackRightYPos = Inches.of(11.3750025);
 
 
     public static final SwerveModuleConstants<TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration> FrontLeft =
